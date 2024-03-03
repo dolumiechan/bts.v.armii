4f1bc23b59553ebc0bd1638dc1167148708f6770
dolumiechan <dolumiechan@gmail.com>
2024-03-03 19:19:21 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+public abstract class Character
+{
+    private string name;
+    private int health;
+    protected Character(string name, int health)
+    {
+        this.name = name;
+        this.health = health;
+    }
+    public virtual void Attack()
+    {
+        Console.WriteLine("Персонаж атакует!");
+    }
+    public virtual void Defend()
+    {
+        Console.WriteLine("Персонаж защищается!");
+    }
+}
+class Warrior : Character
+{
+    public Warrior(string name, int health) : base(name, health)
+    {
+    }
+    public override void Attack()
+    {
+        Console.WriteLine("Мечом пупупу");
+    }
+    public override void Defend()
+    {
+        Console.WriteLine("Щитом пупупу");
+    }
+}
+class Mage : Character
+{
+    public Mage(string name, int health) : base(name, health)
+    {
+
+    }
+    public override void Attack()
+    {
+        Console.WriteLine("Палкой вжух вжух");
+    }
+    public override void Defend()
+    {
+        Console.WriteLine("Защитным барьером пумпум");
+    }
+}
+class Rogue : Character
+{
+    public Rogue(string name, int health) : base(name, health)
+    {
+    }
+    public override void Attack()
+    {
+        Console.WriteLine("Кулаками бумбум");
+    }
+    public override void Defend()
+    {
+        Console.WriteLine("Кулаками бомбом");
+    }
+}
